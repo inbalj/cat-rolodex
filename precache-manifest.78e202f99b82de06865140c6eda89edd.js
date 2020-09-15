f85d2651d7833823b65c01f81578d95e223a4f43
Inbal Jona <inbaljona@hotmail.com>
2020-09-15 11:31:58 +0300

    Updates

---
+++ precache-manifest.78e202f99b82de06865140c6eda89edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c821966e84518cb5e1f1e70a8dc0b9",
+    "url": "/cat-rolodex/index.html"
+  },
+  {
+    "revision": "be5a1a95b38f9fab5014",
+    "url": "/cat-rolodex/static/css/main.a792899b.chunk.css"
+  },
+  {
+    "revision": "ffff86249ce31cd1ed82",
+    "url": "/cat-rolodex/static/js/2.4db0b017.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cat-rolodex/static/js/2.4db0b017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5a1a95b38f9fab5014",
+    "url": "/cat-rolodex/static/js/main.48fe1978.chunk.js"
+  },
+  {
+    "revision": "21a10c21d2651296873f",
+    "url": "/cat-rolodex/static/js/runtime-main.b2b87f6c.js"
+  }
+]);
